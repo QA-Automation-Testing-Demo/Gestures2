ff1516b82ea76320448315999fee39e8f2acee8a
QA-Automation-Testing-Demo <122603762+QA-Automation-Testing-Demo@users.noreply.github.com>
2024-07-29 12:45:50 +0300

    Update DragAndDropTest.cs

--- src/Gestures/DragAndDrop/DragAndDropTest.cs
+++ src/Gestures/DragAndDrop/DragAndDropTest.cs
@@ -29,8 +29,11 @@ namespace DragAndDrop
                 App = appPath,
                 };
 
-             androidOptions.AddAdditionalOption("uiautomator2ServerInstallTimeout", 200000); 
-            androidOptions.AddAdditionalOption("newCommandTimeout", 300); // 5 minutes for new commands
+            var customOptions = new Dictionary<string, object>();
+            customOptions.Add("uiautomator2ServerInstallTimeout", 60000); // 60 seconds timeout
+            customOptions.Add("newCommandTimeout", 300); // 5 minutes for new commands
+
+            androidOptions.AddAdditionalOption("custom:options", customOptions);
             
     _driver = new AndroidDriver(serverUri, androidOptions, TimeSpan.FromMinutes(5)); // Increase command timeout
     _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
